b17433ea83db2d41b6827f59e482a1f95bdbc8f8
Karla <eg25001@esfe.agape.edu.sv>
2025-10-14 22:08:21 -0600

    Ya estan los componentes del Input y ButtonRounded

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Layout } from './components';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { Layout, Input, ButtonRounded } from './components';
 
 export default function App() {
   return (
- <Layout title="Contacto" >
-  
-</Layout>
+    <Layout title="Contacto" >
+      <Input label="Email:" placeholder="name@mail.com" type="email-address" />
+      <Input label="Asunto:" placeholder="Consulta" />
+      <Input label="Mensaje:" placeholder="Escribir aqui..." lines={4}/>
+      <ButtonRounded
+        title="Enviar"
+        onPress={() => Alert.alert('Enviado', 'Mensaje enviado correctamente')}
+      />
+      <ButtonRounded
+        title="Cancelar"
+        isPrimary={false}
+        onPress={() => Alert.alert('Cancelado', 'Envio cancelado')}
+      />
+    </Layout>
   );
 }
-
