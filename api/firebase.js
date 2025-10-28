c4a35e96ba892c705744913e65cadc5813378a84
eg25001-rgb <eg25001@esfe.agape.edu.sv>
2025-10-27 20:40:39 -0600

    api con firebase

---
+++ api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD13MITeo7V2K0EIRCM2Pov9GWsnySSvGo",
+  authDomain: "appserviya-6bcf0.firebaseapp.com",
+  projectId: "appserviya-6bcf0",
+  storageBucket: "appserviya-6bcf0.firebasestorage.app",
+  messagingSenderId: "604266198103",
+  appId: "1:604266198103:web:c77fa6416bad0bbebe5f12"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
